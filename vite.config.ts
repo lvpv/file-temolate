634abd4d88a72dfbd547f52a3c9e9ec885f273de
lvpb <lv941226@gmail.com>
2023-08-01 02:19:47 +0800

    chore(vite.config.ts): 🔨 构建vite基础配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, ConfigEnv, UserConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
+import { wrapperEnv } from './build/getEnv'
+import pkg from './package.json'
+import dayjs from 'dayjs'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), eslintPlugin()]
+const { dependencies, devDependencies, name, version } = pkg
+
+const __APP_INFO__ = {
+  pkg: { dependencies, devDependencies, name, version },
+  lastBuildTime: dayjs().format('YYYY-MM-DD HH:mm:ss')
+}
+// @see: https://vitejs.dev/config/
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const root = process.cwd()
+  const env = loadEnv(mode, root)
+  const viteEnv = wrapperEnv(env)
+  return {
+    base: viteEnv.VITE_PUBLIC_PATH,
+    root,
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src')
+      }
+    },
+    define: {
+      __APP_INFO__: JSON.stringify(__APP_INFO__)
+    },
+    plugins: [vue(), eslintPlugin()]
+  }
 })
